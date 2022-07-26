295203e85d48f7bf9868f499257fed5e2c8e1944
Antifact <soulstormxiii@hotmail.com>
2022-07-26 20:32:16 +1000

    basic navigation to about page

--- lifetree/src/components/App.js
+++ lifetree/src/components/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Navigation from './Navigation';
+import About from './About';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <h1>Lifetree</h1>
       <Navigation />
+
+      {/* Components for routing using React Router */}
+      <Router>
+        <Routes>
+          <Route path="about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
