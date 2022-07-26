8eb9a5694b0ec454c5d9fab980de9865441eb175
Antifact <soulstormxiii@hotmail.com>
2022-07-26 18:04:45 +1000

    clean up files. created nav component

---
+++ lifetree/src/components/Navigation.js
@@ -0,0 +1,13 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <a href="/">Home</a>
+      <a href="/">About</a>
+      <a href="/">Login</a>
+      <a href="/">Register</a>
+      <a href="/">Admin Panel</a>
+    </nav>
+  )
+}
+
+export default Navigation;
