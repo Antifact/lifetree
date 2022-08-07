b504f7463380d39340d42ed02c4f0741eea27072
Antifact <soulstormxiii@hotmail.com>
2022-08-07 15:54:56 +1000

    backend register and login functionality. connected to db and display posts

--- lifetree/backend/app.js
+++ lifetree/backend/app.js
@@ -1,27 +1,35 @@
 const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const router = require('./routes/posts-routes');
-
-const PORT = 4000;
-
-const dbConnection = "mongodb://localhost/lifetree_db"
-
+const posts = require('../src/data/posts');
 const app = express();
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/user-routes');
+const { notFound, errorHandler } = require('./middleware/error');
 
-mongoose.connect(dbConnection, 
-  {},
-  err => {
-    if (err){
-      console.log(e, "Database error");
-    } else {
-      console.log("Connected to DB!")
-    }
-  });
+dotenv.config();
+connectDB();
 
-app.use(cors());
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use('/', router);
+app.use(express.json())
 
-app.listen(PORT, ()=> {console.log(`Server running on port ${PORT}...`)});
+app.get('/', (req, res) => {
+  res.send('API IS RUNNING AGAIN YAYAYA');
+});
+
+app.get('/posts', (req, res) => {
+  res.json(posts);
+});
+
+// app.get('/posts/:id/', (req, res) => {
+//   const post = posts.find((p) => p.id === req.params.id);
+//   res.send(post);
+// });
+
+app.use('/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`Server started on ${PORT}`));
